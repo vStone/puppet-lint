3560b814643f57e961e2de91543bb6e41455b007
Jan Vansteenkiste <jan@vstone.eu>
2012-02-03 07:33:36 +0100

    Collect line_checks and token_checks before hand

---
+++ lib/puppet-lint/plugins/check_split.rb
@@ -0,0 +1,20 @@
+## Temporary checks file for debugging/development purposes
+
+class PuppetLint::Plugins::CheckSplit < PuppetLint::CheckPlugin
+
+  line_check 'contains_vstone' do
+
+
+
+  end
+
+  token_check 'contains_class_vstone' do
+
+    
+
+  end
+
+
+
+
+end
